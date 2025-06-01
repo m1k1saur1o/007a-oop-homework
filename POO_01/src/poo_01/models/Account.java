64c61dd336d5d65aa120818a6d0c7a3b51ce0384
Andrea Rosero <andrea.rosero.p@gmail.com>
2025-06-01 14:58:23 -0400

    convirtiendo a abstracta la clase Account

--- POO_01/src/poo_01/models/Account.java
+++ POO_01/src/poo_01/models/Account.java
@@ -8,12 +8,11 @@ package poo_01.models;
  *
  * @author m1k1
  */
-public class Account {
+public abstract class Account {
     private final String accountNumber;
     private int balance;
     
-   
-    
+   //Constructor Principal    
 
     public Account(String accountNumber) {
         
@@ -21,11 +20,21 @@ public class Account {
             throw new IllegalArgumentException("El numero de cuenta debe tener exactamente 9 digitos");
         }
          
-         
         this.accountNumber = accountNumber;
         this.balance = 0;
     }
 
+    //Constructor sobrecargado con saldo inicial
+    public Account (String accountNumber, int balanceInicial){
+        if (!validarCuenta(accountNumber)) {
+            throw new IllegalArgumentException("El número de cuenta debe tener exactamente 9 dígitos.");
+        }
+        if (balanceInicial < 0) {
+            throw new IllegalArgumentException("El saldo inicial no puede ser negativo.");
+        }
+        this.accountNumber = accountNumber;
+        this.balance = balanceInicial;
+    }
     
     public void mostrarInformacion() {
         System.out.println("Numero de cuenta: " + this.accountNumber);
@@ -41,10 +50,8 @@ public class Account {
     }
     
     private boolean validarCuenta(String accountNumber) {
-        if (accountNumber == null || accountNumber.isEmpty()) {
-            return false;
-        }
-        return accountNumber.length() == 9;
+        
+        return accountNumber != null && accountNumber.length() == 9;
     }
         
     public String getAccountNumber() {
@@ -58,4 +65,8 @@ public class Account {
     public void deposit(int amount) {
         balance += amount;
     }  
+    
+    //Metodo abstracto que cada tipo de cuenta implementara de forma especifica
+    public abstract void realizarOperacion();
+    
 }
