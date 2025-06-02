22c722318e8a605e7f7a5c294aab102c4afd2b6d
m1k1saur1o <miguel.gar.ven@gmail.com>
2025-06-01 20:50:38 -0400

    chore: move menu service methods to a separate service file

--- POO_01/src/poo_01/services/MenuServices.java
+++ POO_01/src/poo_01/services/MenuServices.java
@@ -4,10 +4,125 @@
  */
 package poo_01.services;
 
+import java.util.Scanner;
+import poo_01.models.Client;
+import poo_01.managers.BankAccountManager;
+import poo_01.models.Account;
+
 /**
  *
  * @author m1k1
  */
 public class MenuServices {
     
+    public Client registrarCliente(BankAccountManager bankAccountManager, Scanner scanner, Client client) {
+        System.out.println("\n===== REGISTRO DE CLIENTE =====");
+        System.out.print("Ingrese RUT: ");
+        String rut = scanner.nextLine();
+        
+        // Verificar si el cliente ya existe
+        if (bankAccountManager.buscarCliente(rut) != null) {
+            System.out.println("Error: Ya existe un cliente con ese RUT");
+            return client;
+        }
+        
+        System.out.print("Ingrese nombre: ");
+        String name = scanner.nextLine();
+        System.out.print("Ingrese apellido paterno: ");
+        String firstLastName = scanner.nextLine();
+        System.out.print("Ingrese apellido materno: ");
+        String secondLastName = scanner.nextLine();
+        System.out.print("Ingrese domicilio: ");
+        String address = scanner.nextLine();
+        System.out.print("Ingrese comuna: ");
+        String city = scanner.nextLine();
+        System.out.print("Ingrese telefono: ");
+        String phone = scanner.nextLine();
+        
+        String accountNumber; 
+        
+        while (true) {
+            System.out.print("Ingrese N cuenta corriente (9 digitos): ");
+             accountNumber = scanner.nextLine();
+    
+            if (accountNumber.length() == 9 && accountNumber.matches("\\d+")) {
+                break;  
+            } else {
+                System.out.println("Error: El numero de cuenta debe tener exactamente 9 digitos numericos.");
+            }
+        }
+        
+        
+        Account account = new Account(accountNumber);
+        
+        try {
+            Client nuevoCliente = new Client( rut, name, firstLastName, secondLastName, address, city, phone, account);
+            if (nuevoCliente.registrarCliente() && bankAccountManager.agregarCliente(nuevoCliente)) {
+                System.out.println("Cliente registrado exitosamente");
+                return nuevoCliente;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        return client;
+    }
+    
+    public void verDatosCliente(Client clienteActual) {
+        if (clienteActual == null) {
+            System.out.println("Primero debe registrar un cliente");
+            return;
+        }
+        
+        System.out.println();
+        clienteActual.mostrarDatosCliente();
+    }   
+    
+    public void agregarDeposito(Client clienteActual, Scanner scanner){
+        if(clienteActual == null){
+            System.out.println("Primero debes registrar un cliente.");
+            return;
+        }
+        System.out.println("Ingrese monto a depositar:");
+        int monto = Integer.parseInt(scanner.nextLine());
+        
+        if(monto<=0){
+            System.out.println("El monto debe ser mayor a cero.");
+            return;
+        }
+        
+        clienteActual.getAccount().deposit(monto);
+        System.out.println("Deposito exitoso. Saldo actual: $" + clienteActual.getAccount().getBalance() );
+    }
+        
+    public void realizarGiro(Client clienteActual, Scanner scanner){
+        if(clienteActual == null){
+            System.out.println("Primero debes registrar un cliente.");
+        return;      
+        }
+        
+        System.out.println("Ingrese un monto a retirar: ");
+        int monto = Integer.parseInt(scanner.nextLine());
+        
+        if(monto<=0){
+            System.out.println("El monto debe ser mayor a cero.");
+            return;
+        }
+        
+        boolean exito = clienteActual.getAccount().hacerGiro(monto);
+        if(exito){
+            System.out.println("Giro realizado. Saldo actual $ " + clienteActual.getAccount().getBalance() );
+        } else{
+              System.out.println("Fondos insuficientes. Saldo actual $ " + clienteActual.getAccount().getBalance() );
+        }
+        
+    }
+    
+    public void consultarSaldo(Client clienteActual){
+        if(clienteActual == null){
+            System.out.println("Primero debes registrar un cliente.");
+            return;
+        }
+        System.out.println("Saldo actual $ " + clienteActual.getAccount().getBalance());
+    }
 }
